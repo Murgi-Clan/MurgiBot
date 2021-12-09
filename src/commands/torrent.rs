5ecf5e2e02802b4e21cf8935c034c106cdc053d9
dat-adi <dat.adithya@gmail.com>
2021-12-09 19:42:20 +0530

    Torrents can now be queried
    Created a mechanism to request, clean and respond with torrent links in
    the form of an embedded email.

--- src/commands/torrent.rs
+++ src/commands/torrent.rs
@@ -8,19 +8,36 @@ use serenity::{
         CommandResult
     }
 };
-use rss::Channel;
+
+use rss::{Channel, Item};
 
 #[command]
 async fn torrent(ctx: &Context, msg: &Message) -> CommandResult {
-    let query = env::var("JACKETT_RSS_FEED").expect("Expected an RSS Feed Link");
+    /// This function retrieves the message sent by the user, cleans it
+    /// according to the number of characters in the command, creates a query,
+    /// and then proceeds to retrieve the torrent data from the RSS Feed, and displays
+    /// it in the form of an embed message in Discord.
+    // Retrieving and cleaning the message content
+    let mut s = String::from(&msg.content);
+    let search = s.split_off(10);
+
+    // Creating the query to be sent to Jackett
+    let query = env::var("JACKETT_RSS_FEED").expect("Expected an RSS Feed Link") + &search;
     let content = reqwest::get(&query)
         .await?
         .bytes()
         .await?;
 
-    let channel = Channel::read_from(&content[..])?;
-    println!("{:#?}", channel);
+    // Creates a vector of items, where each item has information of one torrent.
+    let channel:Vec<Item> = Channel::read_from(&content[..])?.items().to_vec();
 
+    // Takes the first 5 responses and pushes them into the Vector.
+    let mut torrs = vec![];
+    for trt in 0..5 {
+        torrs.push((channel[trt].title().unwrap(), channel[trt].link().unwrap(), true));
+    }
+
+    // Creates an embed message
     let msg = msg
         .channel_id
         .send_message(&ctx.http, |m| {
@@ -28,14 +45,10 @@ async fn torrent(ctx: &Context, msg: &Message) -> CommandResult {
             m.embed(|e| {
                 e.title("The Murgi Lords grace you.");
                 e.description("Here's a list of torrents that has been bestowed upon thee.");
-                e.fields(vec![
-                         ("Torrent 1", "magnet:?xt=urn:btih:2549775935A7CC05E771D1871E30BE1F48B440D2&dn=Shadow+of+the+Tomb+Raider%3A+Definitive+Edition+-+Team+Rjaa&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker1.itzmx.com%3A8080%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce", true),
-                         ("Torrent 1", "magnet:?xt=urn:btih:2549775935A7CC05E771D1871E30BE1F48B440D2&dn=Shadow+of+the+Tomb+Raider%3A+Definitive+Edition+-+Team+Rjaa&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker1.itzmx.com%3A8080%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce", true),
-                         ("Torrent 1", "magnet:?xt=urn:btih:2549775935A7CC05E771D1871E30BE1F48B440D2&dn=Shadow+of+the+Tomb+Raider%3A+Definitive+Edition+-+Team+Rjaa&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker1.itzmx.com%3A8080%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce", true),
-                         ("Torrent 1", "magnet:?xt=urn:btih:2549775935A7CC05E771D1871E30BE1F48B440D2&dn=Shadow+of+the+Tomb+Raider%3A+Definitive+Edition+-+Team+Rjaa&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker1.itzmx.com%3A8080%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce", true),
-                         ("Torrent 1", "magnet:?xt=urn:btih:2549775935A7CC05E771D1871E30BE1F48B440D2&dn=Shadow+of+the+Tomb+Raider%3A+Definitive+Edition+-+Team+Rjaa&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker1.itzmx.com%3A8080%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce", true),
-                         ("Torrent 1", "magnet:?xt=urn:btih:2549775935A7CC05E771D1871E30BE1F48B440D2&dn=Shadow+of+the+Tomb+Raider%3A+Definitive+Edition+-+Team+Rjaa&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker1.itzmx.com%3A8080%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce", true),
-                ]);
+                // Need to convert this link into a string.
+                e.url("https://www.youtube.com/watch?v=EGohSsaCJOU");
+                // Probably can't put multiple links to stuff without it looking trash.
+                e.fields(torrs);
                 e.field("Those were some of the top results", "If you didn't get what you want, then, maybe it's not here yet.", false);
                 e.footer(|f| {
                     f.text("Jackett, Torrents, and Murgis.");
@@ -49,6 +62,7 @@ async fn torrent(ctx: &Context, msg: &Message) -> CommandResult {
         })
     .await;
 
+    // Error Handling for the developer
     if let Err(why) = msg {
         println!("Error sending message: {:?}", why);
     }
